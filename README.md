24af022434e4841cd23aa7e1a311b91d9f1a11a7
mapleLeaf <1145365526@qq.com>
2024-11-14 15:08:30 +0800

    feat: 实现核心场景框架

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Celebrity-Brawl-of-Plants" 
---
+++ selector_scene.h
@@ -0,0 +1,18 @@
+#ifndef _SELECTOR_SCENE_H_
+#define _SELECTOR_SCENE_H_
+
+#include "scene.h"
+#include "scene_manager.h"
+
+class SelectorScene : public Sceen;
+{
+public:
+	SelectorScene() = default;
+	~SelectorScene() = default;
+
+private:
+
+};
+
+
+#endif // !_SELECTOR_SCENE_H_
